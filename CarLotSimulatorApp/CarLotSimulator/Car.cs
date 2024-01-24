79e4aa75c9808c9c99709f68044924282643a272
JustinBenning <justin.benning@gmail.com>
2024-01-24 18:22:29 -0500

    upload of exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+//Create a seperate class file called Car
+public class Car
+{
+    public Car()
+    {
+
+    }
+    //Create a seperate class file called Car
+    //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+    public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+    {
+        Year = year;
+        Make = make;
+        Model = model;
+        EngineNoise = engineNoise;
+        HonkNoise = honkNoise;
+        IsDriveable = isDriveable;
+
+    }
+    public int Year { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string EngineNoise { get; set; }
+    public string HonkNoise { get; set; }
+    public bool IsDriveable { get; set; }
+
+    //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+    public void MakeEngineNoise(string engineNoise)
+    {
+        Console.WriteLine(engineNoise);
+    }
+    public void MakeHonkNoise( string honkNoise)
+    {
+        Console.WriteLine(honkNoise);
+    }
+    /*Car car1 = new Car(1965, "Shelby", "Mustang", "Vroom", "BeepBeep", true)
+    {
+
+    };
+    Car car2 = new Car(1998, "Honda", "Accord", "Purrrr", "WompWomp", false)
+    {
+
+    };
+    Car car3 = new Car(2023, "Chevy", "Corvette", "Whirrrrr", "MEEEEEEEEEP", true)
+    {
+
+    };*/
+    
+
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CarLotSimulator
 {
@@ -8,10 +9,29 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Now that the Car class is created we can instanciate 3 new cars
+            //Set the properties for each of the cars
+            Car car1 = new Car(1965, "Shelby", "Mustang", "Vroom", "BeepBeep", true)
+            {
+                
+            };
+            Car car2 = new Car(1998, "Honda", "Accord", "Purrrr", "WompWomp", false)
+            {
+
+            };
+            Car car3 = new Car(2023, "Chevy", "Corvette", "Whirrrrr", "MEEEEEEEEEP", true)
+            {
+                
+            };
+            Console.WriteLine("Car 1");
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+            Console.WriteLine("Car 2");
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+            Console.WriteLine("Car 3");
+            car3.MakeEngineNoise(car3.EngineNoise);
+            car3.MakeHonkNoise(car3.HonkNoise);
 
 
             //Now that the Car class is created we can instanciate 3 new cars
@@ -30,4 +50,7 @@ namespace CarLotSimulator
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
     }
+    
+    
+
 }
